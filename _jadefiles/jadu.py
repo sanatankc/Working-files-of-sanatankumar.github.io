e9d2207e158a7a297c2e69fd556373753a113eb5
Sanatan kumar <sanatankumarchaudhary@gmail.com>
2017-02-03 07:12:26 +0530

    initial commit

---
+++ _jadefiles/jadu.py
@@ -0,0 +1,11 @@
+import turtle
+window = turtle.Screen()
+fred = turtle.Pen()
+
+#TO-Do-list
+
+#1. Make nav toggle working
+#1. Increase Mobile Responsiveness
+#1. Include Contact Sections
+#1. 
+W 
---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$(function() {
+    $( ".mobile-nav-toggle" ).click(function() {
+    $('.mobile-nav-toggle').toggleClass('open');
+    $('nav').toggleClass('open');
+});
+});
